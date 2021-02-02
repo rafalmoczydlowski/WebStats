db83f31781e62af6a25f45f421991b5ca65236e8
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-02-02 18:34:32 +0100

    JSP support

--- src/main/java/com/rafinha/webstats/api/LoginController.java
+++ src/main/java/com/rafinha/webstats/api/LoginController.java
@@ -2,14 +2,12 @@ package com.rafinha.webstats.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    @ResponseBody
     public String getLoginMessage() {
-        return "Witaj w programie WebStats! Zmodyfikowano";
+        return "login";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>WebStats</title>
+</head>
+<body>
+My First JSP
+</body>
+</html>
