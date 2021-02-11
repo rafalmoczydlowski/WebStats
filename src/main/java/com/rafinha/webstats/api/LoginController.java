5a85e44f6031b4c0f80a7b7fbce266cc525ae081
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-02-11 17:12:21 +0100

    Remove unused imports

--- src/main/java/com/rafinha/webstats/api/LoginController.java
+++ src/main/java/com/rafinha/webstats/api/LoginController.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
