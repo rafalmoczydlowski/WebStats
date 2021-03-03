db6847d54364dc0feda6b2de092ced4593892eba
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-03-03 19:11:40 +0100

    JPA and refactoring

--- src/main/java/com/rafinha/webstats/jpa/UserCommandLineRunner.java
+++ src/main/java/com/rafinha/webstats/jpa/UserCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.rafinha.webstats.jpa;
 
+import com.rafinha.webstats.service.UserDAOService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,29 +10,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserCommandLineRunner implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(UserCommandLineRunner.class);
+    private static final Logger logOne = LoggerFactory.getLogger(UserCommandLineRunner.class); //first method
+    private static final Logger logSecond = LoggerFactory.getLogger(UserDAOService.class); //second method
 
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private UserDAOService userDAOService;
+
     @Override
     public void run(String... args) throws Exception {
+        //first method
         repository.save(new User("Rafał","Admin"));
         repository.save(new User("Paweł","User"));
         repository.save(new User("Krzysztof","User"));
 
         for (User user : repository.findAll()) {
-            log.info(user.toString());
+            logOne.info(user.toString());
         }
-        log.info("=============================");
-        log.info("Website administrators: ");
+        logOne.info("=============================");
+        logOne.info("Website administrators: ");
         for (User user : repository.findByRole("Admin")) {
-            log.info(user.toString());
+            logOne.info(user.toString());
         }
-        log.info("=============================");
-        log.info("Website users: (alphabetic order)");
+        logOne.info("=============================");
+        logOne.info("Website users: (alphabetic order)");
         for (User user : repository.findByRoleOrderByName("User")) {
-            log.info(user.toString());
+            logOne.info(user.toString());
         }
+
+        //second method
+        User user = new User("Jacek", "User");
+        long insert = userDAOService.insertUser(user);
+        logSecond.info("New User is created: " + user + " with ID: " + insert);
     }
 }
